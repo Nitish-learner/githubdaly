8674458a5e5abbe7694c55d786ec7e4a47b1fc1d
Nitish <nitishkumarpiprakhura2005@gmail.com>
2026-07-28 16:11:44 +0530

    Day 3: Add Scanner input

--- Day-03-java/ReverseString.java
+++ Day-03-java/ReverseString.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // Reverse String Program
 // Author: Nitish Kumar
 
@@ -5,15 +7,20 @@ public class ReverseString {
 
     public static void main(String[] args) {
 
-        String text = "Java";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+
         String reverse = "";
 
-        // Reverse the string
         for (int i = text.length() - 1; i >= 0; i--) {
             reverse += text.charAt(i);
         }
 
         System.out.println("Original String: " + text);
         System.out.println("Reversed String: " + reverse);
+
+        sc.close();
     }
 }
