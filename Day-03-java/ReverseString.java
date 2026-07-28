55c9600a330fd291d49e0dd76799429959e6a4b1
Nitish <nitishkumarpiprakhura2005@gmail.com>
2026-07-28 15:57:46 +0530

    Day 3: Reverse String in Java

---
+++ Day-03-java/ReverseString.java
@@ -0,0 +1,15 @@
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        String text = "Java";
+        String reverse = "";
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reverse += text.charAt(i);
+        }
+
+        System.out.println("Original String: " + text);
+        System.out.println("Reversed String: " + reverse);
+    }
+}
