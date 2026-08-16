bb93e7f0b645da385e8f0f77b3135f625548e1be
Nitish <nitishkumarpiprakhura2005@gmail.com>
2026-08-16 09:45:28 +0530

    Day 14: Find Second Largest Element

---
+++ Day-14-java/SecondLargest.java
@@ -0,0 +1,28 @@
+public class SecondLargest {
+
+    public static int findSecondLargest(int[] arr) {
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        return secondLargest;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 5, 8, 20, 15};
+
+        int result = findSecondLargest(arr);
+
+        System.out.println("Second Largest Element: " + result);
+    }
+}
