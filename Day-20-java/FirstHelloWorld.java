5dfdcb83a7882e5cbf3c1db5f463dcf274b7cf12
Nitish <nitishkumarpiprakhura2005@gmail.com>
2026-09-09 18:22:13 +0530

    Day 1: Java Hello World

---
+++ Day-20-java/FirstHelloWorld.java
@@ -0,0 +1,10 @@
+public class FirstHelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Welcome");
+        System.out.println("Nitish");
+
+    }
+}
